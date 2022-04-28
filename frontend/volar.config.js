61b901c3d8a6e3f6b42a81472f2b02bfdae530c0
Jun Kajii <junk4jii@gmail.com>
2022-04-28 20:04:39 -0300

    :wrench: setup prettier with volar

---
+++ frontend/volar.config.js
@@ -0,0 +1,13 @@
+/** @type {import('@volar-plugins/prettier')} */
+const { volarPrettierPlugin } = require('@volar-plugins/prettier')
+
+module.exports = {
+  plugins: [
+    volarPrettierPlugin({
+      languages: ['html', 'css', 'scss', 'typescript', 'javascript'],
+      html: {
+        breakContentsFromTags: true,
+      },
+    }),
+  ],
+}
